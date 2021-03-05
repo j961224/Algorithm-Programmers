d3672272444c3bb89457831044d3f0b0aa9b4276
정유석 <j961224@naver.com>
2021-03-05 23:12:36 +0900

    Programmers 연습문제 Source code

---
+++ 자릿수 더하기/Solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        while(true){
+            if(n/10==0){
+                answer+=n%10;
+                break;
+            }
+            answer+=n%10;
+            n/=10;
+        }
+
+        return answer;
+    }
+}
